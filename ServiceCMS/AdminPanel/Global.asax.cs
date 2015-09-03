8dcf9605e207b503286347fc6aff54ebdc5fca65
Artur Stelmach <arturstelmach92@gmail.com>
2015-09-03 21:45:08 +0200

    Bootstrap, AngularJS and Autofac installed

--- ServiceCMS/AdminPanel/Global.asax.cs
+++ ServiceCMS/AdminPanel/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdminPanel.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -23,6 +24,8 @@ namespace AdminPanel
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+            var autofac = new AutofacConfig();
+            autofac.Configure();
         }
     }
 }
---
+++ ServiceCMS/DIRegister/DIRegister.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIRegister
+{
+    public class DIRegister
+    {
+        public static void Register(ContainerBuilder builder)
+        {
+            RegisterCore(builder);
+            RegisterLogic(builder);
+            RegisterModules(builder);
+            RegisterJobs(builder);
+        }
+
+        private static void RegisterCore(ContainerBuilder builder)
+        {
+
+        }
+
+        private static void RegisterLogic(ContainerBuilder builder)
+        {
+
+        }
+
+        private static void RegisterModules(ContainerBuilder builder)
+        {
+
+        }
+        private static void RegisterJobs(ContainerBuilder builder)
+        {
+
+        }
+    }
+}
