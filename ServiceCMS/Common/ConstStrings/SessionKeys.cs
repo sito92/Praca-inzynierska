67f7496b41f4c56be6f17527e167b5261b504d24
Artur Stelmach <arturstelmach92@gmail.com>
2015-09-04 17:28:06 +0200

    Added AuthorizeAttribute

---
+++ ServiceCMS/Common/ConstStrings/SessionKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.ConstStrings
+{
+    public static class SessionKeys
+    {
+        public const string USER_VIEW_MODEL = "USER_VIEW_MODEL";
+    }
+}
