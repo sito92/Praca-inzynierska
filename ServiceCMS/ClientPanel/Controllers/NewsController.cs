1ac9acdacafeec9123e8f02cbb4d95687c65de42
Kamil Ślusarczyk <kamil.slusarczyk@hotmail.com>
2015-12-27 11:58:35 +0100

    NewsController in ClientPanel

---
+++ ServiceCMS/ClientPanel/Controllers/NewsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Logic.News.Interfaces;
+
+namespace ClientPanel.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly INewsService _newsService;
+
+        public NewsController(INewsService newsService)
+        {
+            _newsService = newsService;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetLatestNews(int amount,int page)
+        {
+            var resultCollection = _newsService.GetNewestNewsesCollection().OrderBy(x => x.LastModifiedTimeStamp).Take(amount*page).Skip(amount*page-amount).ToList();
+
+            if(resultCollection.Any())
+                return Json(new { success = true, data = resultCollection }, JsonRequestBehavior.AllowGet);
+            else
+                return Json(new { success = false }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetNewsCount()
+        {
+            var newsAmount = _newsService.GetNewestNewsesCollection().Count();
+
+            if (newsAmount > 0)
+                return Json(new { success = true, data = newsAmount }, JsonRequestBehavior.AllowGet);
+            else
+                return Json(new { success = false }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
