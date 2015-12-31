4010c1a7e163987b5da61d0839a15a3fdc7dac13
Artur Stelmach <arturstelmach92@gmail.com>
2015-12-31 10:38:34 +0100

    FileDownload

--- ServiceCMS/ClientPanel/Controllers/FileController.cs
+++ ServiceCMS/ClientPanel/Controllers/FileController.cs
@@ -1,27 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ClientPanel.Extensions;
 using Logic.File.Interfaces;
+using Modules.FileManager.Interfaces;
 
 namespace ClientPanel.Controllers
 {
     public class FileController : Controller
     {
         private readonly IFileService _fileService;
-
-        public FileController(IFileService fileService)
+        private readonly IFileManager _fileManager;
+        public FileController(IFileService fileService,IFileManager fileManager)
         {
             _fileService = fileService;
+            _fileManager = fileManager;
         }
 
         public ActionResult Download(int fileId)
         {
-            //var response = _fileService.Download(fileId);
-
-            return new JsonNetResult(new { success = true }, JsonRequestBehavior.AllowGet);
+            var fileModel = _fileService.GetById(fileId);
+            if (fileModel!=null)
+            {
+                var fileData = _fileManager.GetFileData(fileModel.Path);
+                if (fileData != null)
+                {
+                    var pathFileName = Path.GetFileName(fileModel.Path);
+                    var minme = MimeMapping.GetMimeMapping(pathFileName);
+                    return File(fileData, minme, pathFileName);
+                }
+            }
+            return View("Error");
+           
         }
 
     }
